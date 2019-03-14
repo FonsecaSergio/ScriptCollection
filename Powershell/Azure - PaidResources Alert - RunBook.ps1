ab290ce97a0888312a85f2531a7896aa7f86aee0
Sergio Fonseca <sefonsec@microsoft.com>
2019-03-14 14:39:24 +0000

    Update Azure - PaidResources Alert - RunBook.ps1

--- Powershell/Azure - PaidResources Alert - RunBook.ps1
+++ Powershell/Azure - PaidResources Alert - RunBook.ps1
@@ -1,13 +1,14 @@
-﻿#param(
-#    [Parameter (Mandatory = $true)]
-#    [string]$SubscriptionName,
-#    #SEFONSEC Microsoft Azure Internal Consumption
-#
-#    [Parameter (Mandatory = $false)]
-#    [System.Collections.ArrayList]$IgnoreResGroups
-#    #On Azure Runbook send as JSON format ['LogAnalytics','MIJumpbox','NetworkWatcherRG']
-#)
+﻿<#
+param(
+    [Parameter (Mandatory = $true)]
+    [string]$SubscriptionName,
+    #SEFONSEC Microsoft Azure Internal Consumption
 
+    [Parameter (Mandatory = $false)]
+    [System.Collections.ArrayList]$IgnoreResGroups
+    #On Azure Runbook send as JSON format ['LogAnalytics','MIJumpbox','NetworkWatcherRG']
+
+#>
 $ErrorActionPreference = "Stop"
 
 Import-Module Az.Accounts
@@ -19,22 +20,22 @@ try{
 } catch {}
 
 
-#Enable for alert https://docs.microsoft.com/en-us/azure/automation/automation-alert-metric
+<#Enable for alert https://docs.microsoft.com/en-us/azure/automation/automation-alert-metric#>
 
 
-
-##########################################################################################################################################################
+<##########################################################################################################################################################
 #Parameters
-##########################################################################################################################################################
+##########################################################################################################################################################>
 
+<#
+[string]$SubscriptionName = "SEFONSEC Microsoft Azure Internal Consumption"
 
-#[string]$SubscriptionName = "SEFONSEC Microsoft Azure Internal Consumption"
-
-#[System.Collections.ArrayList]$IgnoreResGroups = @(
-#    "LogAnalytics", 
-#    "MIJumpbox",
-#    "NetworkWatcherRG"
-#)
+[System.Collections.ArrayList]$IgnoreResGroups = @(
+    "LogAnalytics", 
+    "MIJumpbox",
+    "NetworkWatcherRG"
+)
+#>
 
 [System.Collections.ArrayList]$IgnoreAzureResourcesTypesFree = @(
     "microsoft.insights/alertrules",
@@ -49,23 +50,24 @@ try{
 
 
 
-##########################################################################################################################################################
+<##########################################################################################################################################################
 #Connect
-##########################################################################################################################################################
-#Clear-Host
-#Disconnect-AzAccount
+##########################################################################################################################################################>
+<#
+    Clear-Host
+    Disconnect-AzAccount
+#>
 
 $context = Get-AzContext 
 
 if ($context -eq $null)
 {
-    #Connect-AzAccount
     Connect-AzAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint -Subscription $Conn.SubscriptionId
 }
 
-##########################################################################################################################################################
+<##########################################################################################################################################################
 #Get Resources Groups / Remove  Ignorables
-##########################################################################################################################################################
+##########################################################################################################################################################>
 [System.Collections.ArrayList]$AzureResourceGroups = Get-AzResourceGroup
 $AzureResourceGroups = @($AzureResourceGroups | Where-Object {$_.ResourceGroupName -notin $IgnoreResGroups})
 
@@ -76,9 +78,9 @@ Write-Output ($AzureResourceGroups | Select ResourceGroupName | Out-String)
 
 
 
-##########################################################################################################################################################
+<##########################################################################################################################################################
 #Get Resources / Remove Ignorables
-##########################################################################################################################################################
+##########################################################################################################################################################>
 [System.Collections.ArrayList]$AzureResources = Get-AzResource
 
 $AzureResources = @($AzureResources | Where-Object {$_.ResourceGroupName -notin $IgnoreResGroups})
@@ -93,9 +95,9 @@ Write-Output ($AzureResources | Select Type, ResourceGroupName, Name, ParentReso
 
 
 
-##########################################################################################################################################################
+<##########################################################################################################################################################
 #Get Databases / Remove Ignorables
-##########################################################################################################################################################
+##########################################################################################################################################################>
 Write-Output "---------------------------------------------------------------------------------------------------------------"
 Write-Output "Get Databases / Remove Ignorables"
 Write-Output "---------------------------------------------------------------------------------------------------------------"
@@ -117,7 +119,7 @@ foreach ($database in $AzureDatabases)
     {
         $databaseObject = Get-AzSqlDatabase -ResourceGroupName $database.ResourceGroupName -ServerName $ServerName -DatabaseName $DatabaseName
         
-        #Database basic are cheap - Ignore
+        <#Database basic are cheap - Ignore#>
         if ($databaseObject.SkuName -eq "Basic")
         {
             $AzureDatabasesToIgnore += $database
@@ -125,8 +127,7 @@ foreach ($database in $AzureDatabases)
 
         try
         {
-            #$databaseObject.DatabaseName
-            #Database containg tag "Ignore"
+            <#Database containg tag "Ignore"#>
             if ($databaseObject.Tags.ContainsKey("Ignore").ToString() -eq "true")
             {
                 $AzureDatabasesToIgnore += $database
@@ -145,9 +146,10 @@ foreach ($database in $AzureDatabasesToIgnore)
 
 Write-Output "Removed ($($AzureDatabasesToIgnore.Count) / $($AzureDatabases.Count)) databases"
 Write-Output ""
-##########################################################################################################################################################
+
+<##########################################################################################################################################################
 #Get StorageAccounts / Remove Ignorables
-##########################################################################################################################################################
+##########################################################################################################################################################>
 Write-Output "---------------------------------------------------------------------------------------------------------------"
 Write-Output "Get StorageAccounts / Remove Ignorables"
 Write-Output "---------------------------------------------------------------------------------------------------------------"
@@ -160,7 +162,7 @@ foreach ($StorageAccount in $AzureStorageAccounts)
 {
     $StorageAccountObject = Get-AzStorageAccount -ResourceGroupName $StorageAccount.ResourceGroupName -Name $StorageAccount.Name
     
-    #Storage Account standard are cheap - Ignore
+    <#Storage Account standard are cheap - Ignore#>
     if ($StorageAccountObject.Sku.Tier -eq "Standard")
     {
         $AzureStorageToIgnore += $StorageAccount
@@ -174,7 +176,8 @@ foreach ($StorageAccount in $AzureStorageToIgnore)
 
 Write-Output "Removed ($($AzureStorageToIgnore.Count) / $($AzureStorageAccounts.Count)) storage accounts"
 Write-Output ""
-##########################################################################################################################################################
+
+<##########################################################################################################################################################>
 [System.Collections.ArrayList]$ResourcesAlert = @()
 $ResourcesAlert = @($AzureResources | Select Type, ResourceGroupName, Name, ParentResource | Out-String)
 
@@ -183,9 +186,6 @@ Write-Output "Check this resources"
 Write-Output "---------------------------------------------------------------------------------------------------------------"
 Write-Output ($ResourcesAlert)
 Write-Output "---------------------------------------------------------------------------------------------------------------"
-
-#New-AzSqlDatabase -DatabaseName StandardTest -ResourceGroupName CSSAzureDB -ServerName Fonsecanet -Edition Standard -RequestedServiceObjectiveName S0
-#Get-AzSqlDatabase  -DatabaseName StandardTest -ResourceGroupName CSSAzureDB -ServerName Fonsecanet | Set-AzSqlDatabase -Tags @{Ignore="true"}
 
 if($ResourcesAlert.Count -ge 1)
 {
