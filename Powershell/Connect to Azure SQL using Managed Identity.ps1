86d2a01727e441bc37661ea12b2ea97114da4ba8
Sérgio Fonseca <sergio@fonsecanet.com>
2020-04-07 14:50:14 +0100

    Update Connect to Azure SQL using Managed Identity.ps1
    Update

--- Powershell/Connect to Azure SQL using Managed Identity.ps1
+++ Powershell/Connect to Azure SQL using Managed Identity.ps1
@@ -4,23 +4,33 @@
 #GRANT EXECUTE TO [VMSQLCLient]
 
 
+
 $response = Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fdatabase.windows.net%2F' -Method GET -Headers @{Metadata="true"}
-
 $content = $response.Content | ConvertFrom-Json
-
 $AccessToken = $content.access_token
 
 $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
-$SqlConnection.ConnectionString = "Data Source = fonsecanet.database.windows.net; Initial Catalog = sandbox"
+$SqlConnection.ConnectionString = "Server=tcp:fonsecanet1.database.windows.net,1433;Initial Catalog=sandbox;Persist Security Info=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=5;"
 $SqlConnection.AccessToken = $AccessToken
-$SqlConnection.Open()
 
-$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
-$SqlCmd.CommandText = "SELECT @@SERVERNAME;"
-$SqlCmd.Connection = $SqlConnection
-$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
-$SqlAdapter.SelectCommand = $SqlCmd
-$DataSet = New-Object System.Data.DataSet
-$SqlAdapter.Fill($DataSet)
+try
+{
+    $SqlConnection.Open()
 
-$SqlConnection.Close()
+    $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+    $SqlCmd.CommandText = "SELECT SERVERNAME = @@SERVERNAME;"
+    $SqlCmd.Connection = $SqlConnection
+    $SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+    $SqlAdapter.SelectCommand = $SqlCmd
+    $DataSet = New-Object System.Data.DataSet
+    $SqlAdapter.Fill($DataSet) | Out-Null
+
+    Write-Host "Success Connection @@SERVERNAME = $($DataSet.Tables[0].Rows[0].SERVERNAME)" -ForegroundColor Green
+
+    $SqlConnection.Close()
+}
+catch
+{
+  Write-Host "An error occurred:" -ForegroundColor Red
+  Write-Host $_ -ForegroundColor Red
+}
