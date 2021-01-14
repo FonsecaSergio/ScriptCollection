c2319097fbfb85adbb3c4ec4a267d5c4407ace6d
Sérgio C Fonseca <sergio@fonsecanet.com>
2021-01-14 20:02:43 +0000

    Added statistics at end

--- Powershell/Split Fotos By Date.ps1
+++ Powershell/Split Fotos By Date.ps1
@@ -9,37 +9,42 @@
    
 .DESCRIPTION    
  
-.PARAMETER folderPath path where images and videos will be searched
+.PARAMETER folderPath 
+    path where images and videos will be searched
+    **By Design not recursive**
        
-.PARAMETER DEBUG true just simulate process
+.PARAMETER DEBUG 
+    if true = just simulate process
        
 #> 
 
+$ErrorActionPreference = "Stop"
+
 Clear-Host
-$folderPath = "D:\OneDrive_Personal\OneDrive\Pictures\Imagens\TEMP\iPhone Sergio"
-$DEBUG = $false
+[string]$folderPath = "D:\OneDrive_Personal\OneDrive\Pictures\Imagens\TEMP\iPhone Sergio"
+[bool]$DEBUG = $true
 
 
-#https://devblogs.microsoft.com/scripting/use-powershell-to-find-metadata-from-photograph-files/
+########################################################################################################
+Function Get-FileMetaData { 
+    # ----------------------------------------------------------------------------- 
+    # https://devblogs.microsoft.com/scripting/use-powershell-to-find-metadata-from-photograph-files/
+    # Script: Get-FileMetaDataReturnObject.ps1 
+    # Author: ed wilson, msft 
+    # Date: 01/24/2014 12:30:18 
+    # Keywords: Metadata, Storage, Files 
+    # comments: Uses the Shell.APplication object to get file metadata 
+    # Gets all the metadata and returns a custom PSObject 
+    # it is a bit slow right now, because I need to check all 266 fields 
+    # for each file, and then create a custom object and emit it. 
+    # If used, use a variable to store the returned objects before attempting 
+    # to do any sorting, filtering, and formatting of the output. 
+    # To do a recursive lookup of all metadata on all files, use this type 
+    # of syntax to call the function: 
+    # Get-FileMetaData -folder (gci e:\music -Recurse -Directory).FullName 
+    # note: this MUST point to a folder, and not to a file. 
+    # ----------------------------------------------------------------------------- 
 
-# ----------------------------------------------------------------------------- 
-# Script: Get-FileMetaDataReturnObject.ps1 
-# Author: ed wilson, msft 
-# Date: 01/24/2014 12:30:18 
-# Keywords: Metadata, Storage, Files 
-# comments: Uses the Shell.APplication object to get file metadata 
-# Gets all the metadata and returns a custom PSObject 
-# it is a bit slow right now, because I need to check all 266 fields 
-# for each file, and then create a custom object and emit it. 
-# If used, use a variable to store the returned objects before attempting 
-# to do any sorting, filtering, and formatting of the output. 
-# To do a recursive lookup of all metadata on all files, use this type 
-# of syntax to call the function: 
-# Get-FileMetaData -folder (gci e:\music -Recurse -Directory).FullName 
-# note: this MUST point to a folder, and not to a file. 
-# ----------------------------------------------------------------------------- 
-Function Get-FileMetaData 
-{ 
     <# 
     .Synopsis 
     This function gets file metadata and returns it as a custom PS Object  
@@ -78,65 +83,66 @@ Function Get-FileMetaData
     #Requires -Version 2.0 
     #> 
     Param([string[]]$folder) 
-    foreach($sFolder in $folder) 
-    { 
+    foreach ($sFolder in $folder) { 
         $a = 0 
         $objShell = New-Object -ComObject Shell.Application 
         $objFolder = $objShell.namespace($sFolder) 
  
-        foreach ($File in $objFolder.items()) 
-        {  
+        foreach ($File in $objFolder.items()) {  
     
             $FileMetaData = New-Object PSOBJECT 
-            for ($a ; $a  -le 266; $a++) 
-            {  
-                if($objFolder.getDetailsOf($File, $a)) 
-                { 
-                    $hash += @{$($objFolder.getDetailsOf($objFolder.items, $a))  = 
-                    $($objFolder.getDetailsOf($File, $a)) } 
+            for ($a ; $a -le 266; $a++) {  
+                if ($objFolder.getDetailsOf($File, $a)) { 
+                    $hash += @{$($objFolder.getDetailsOf($objFolder.items, $a)) = 
+                        $($objFolder.getDetailsOf($File, $a)) 
+                    } 
                     $FileMetaData | Add-Member $hash 
                     $hash.clear()  
                 } #end if 
             } #end for  
-            $a=0 
+            $a = 0 
             $FileMetaData          
         } #end foreach $file 
     } #end foreach $sfolder 
 } #end Get-FileMetaData
 
-function Get-AsciiFromUnicode
-{
+########################################################################################################
+function Get-AsciiFromUnicode {
     Param([string]$UnicodeString)
 
-    $encoding= [System.Text.Encoding]::ASCII
+    $encoding = [System.Text.Encoding]::ASCII
     $uencoding = [System.Text.Encoding]::UNICODE
-    [System.Text.Encoding]::Convert([System.Text.Encoding]::UNICODE, $encoding, $uencoding.GetBytes($DateTaken)) | % { $myStr += [char]$_}
-    $myStr = ($myStr.Replace("?",""))
+    [System.Text.Encoding]::Convert([System.Text.Encoding]::UNICODE, $encoding, $uencoding.GetBytes($DateTaken)) | % { $myStr += [char]$_ }
+    $myStr = ($myStr.Replace("?", ""))
     $myStr
 } #end Get-AsciiFromUnicode
+########################################################################################################
 
-#####################################################################################################################
+
+########################################################################################################
+
 #Remove-Variable -Name "Files"
 
-if ($Files -eq $null)
-{ 
-     if ($DEBUG)
-     { Write-Host "Get-FileMetaData ($($folderPath))" -ForegroundColor Yellow }
+if ($Files -eq $null) { 
+    if ($DEBUG)
+    { Write-Host "Get-FileMetaData ($($folderPath))" -ForegroundColor Yellow }
 
-     $Files = Get-FileMetaData -folder $folderPath
+    $Files = Get-FileMetaData -folder $folderPath
+    $Files = $Files | Where-Object -Property Kind -ne "Folder"
 }
-else
-{ 
+else { 
     if ($DEBUG)
     { Write-Host "Already got file metadata ($($folderPath))" -ForegroundColor Yellow }
     
 }
 
-if ($DEBUG)
-{
+if ($DEBUG) {
     $Files | Out-GridView
 }
 
+
+[int]$QtFiles = $Files.Count
+[int]$QtFilesCopied = 0
 
 #####################################################################################################################
 
@@ -147,28 +153,28 @@ Write-Host "--------------------------------------------------------------------
 #####################################################################################################################
 $picMetadata = $Files | Where-Object -Property Kind -eq "Picture"
 
-foreach ($picture in $picMetadata)
-{
+[int]$QtFilesImages = $picMetadata.Count
+
+foreach ($picture in $picMetadata) {
     ##########################################################################
     Write-Host "---------------------------------------------------" -ForegroundColor Red
     Write-Host "Name ($($picture.Name))" -ForegroundColor Red
 
     ##########################################################################
-    $DateCreated = ($picture.'Date created').Substring(0,10)
+    $DateCreated = ($picture.'Date created').Substring(0, 10)
     $DateCreated = [datetime]::parseexact($DateCreated, 'dd/MM/yyyy', $null)
     Write-Host " - DateCreated ($($DateCreated.ToString("yyyy-MM-dd")))" -ForegroundColor DarkYellow
 
     ##########################################################################
-    $DateModified = ($picture.'Date modified').Substring(0,10)
+    $DateModified = ($picture.'Date modified').Substring(0, 10)
     $DateModified = [datetime]::parseexact($DateModified, 'dd/MM/yyyy', $null)
     Write-Host " - DateModified ($($DateModified.ToString("yyyy-MM-dd")))" -ForegroundColor DarkYellow
     ##########################################################################
     $FolderNameToCreate = "NODATEINFO"
     ##########################################################################
 
-    if (($picture.'Date taken') -ne $null)
-    {
-        $DateTaken = ($picture.'Date taken').Substring(0,13)
+    if (($picture.'Date taken') -ne $null) {
+        $DateTaken = ($picture.'Date taken').Substring(0, 13)
         $DateTaken = Get-AsciiFromUnicode -UnicodeString $DateTaken
 
         #$DateTaken
@@ -178,14 +184,13 @@ foreach ($picture in $picMetadata)
         ##########################################################################
         $FolderNameToCreate = $DateTaken
     }
-    else
-    {           
+    else {           
         Write-Host " - DateTaken (NULL)" -ForegroundColor DarkYellow
 
         if ($DateCreated -gt $DateModified)
-            { $FolderNameToCreate = $DateCreated }
+        { $FolderNameToCreate = $DateCreated }
         else
-            { $FolderNameToCreate = $DateModified}
+        { $FolderNameToCreate = $DateModified }
     }
 
     ################################################################################################
@@ -194,22 +199,22 @@ foreach ($picture in $picMetadata)
     Write-Host "  * FolderToCreate ($($FolderNameToCreate))" -ForegroundColor Yellow
 
     $PathFolderToCreate = "$($folderPath)\$($FolderNameToCreate)"
-    $PathFolderToCreate = $PathFolderToCreate.Replace("\\","\")
+    $PathFolderToCreate = $PathFolderToCreate.Replace("\\", "\")
 
     ################################################################################################
-    if (!(Test-Path -Path $PathFolderToCreate))
-    {
+    if (!(Test-Path -Path $PathFolderToCreate)) {
         if (!($DEBUG))
-            { New-Item -Path $PathFolderToCreate -ItemType directory | Out-Null }
+        { New-Item -Path $PathFolderToCreate -ItemType directory | Out-Null }
         else
-            { Write-Host "  - New-Item -Path $($PathFolderToCreate) -ItemType directory | Out-Null" -ForegroundColor Gray }
+        { Write-Host "  - New-Item -Path $($PathFolderToCreate) -ItemType directory | Out-Null" -ForegroundColor Gray }
     }
 
     ################################################################################################
     Write-Host "  * Copy-Item -Path $($picture.Path) -Destination $($PathFolderToCreate)" -ForegroundColor Yellow
-
     if (!($DEBUG))
-        { Copy-Item -Path $picture.Path -Destination $PathFolderToCreate }
+    { Copy-Item -Path $picture.Path -Destination $PathFolderToCreate }
+    
+    $QtFilesCopied += 1
     ################################################################################################
 }
 
@@ -229,27 +234,27 @@ Write-Host "--------------------------------------------------------------------
 #####################################################################################################################
 $vidMetadata = $Files | Where-Object -Property Kind -eq "Video"
 
-foreach ($video in $vidMetadata)
-{
+[int]$QtFilesVideos = $vidMetadata.Count
+
+foreach ($video in $vidMetadata) {
     Write-Host "---------------------------------------------------" -ForegroundColor Red
     Write-Host "Name ($($video.Name))" -ForegroundColor Red
 
     ##########################################################################
-    $DateCreated = ($video.'Date created').Substring(0,10)
+    $DateCreated = ($video.'Date created').Substring(0, 10)
     $DateCreated = [datetime]::parseexact($DateCreated, 'dd/MM/yyyy', $null)
     Write-Host " - DateCreated ($($DateCreated.ToString("yyyy-MM-dd")))" -ForegroundColor DarkYellow
 
     ##########################################################################
-    $DateModified = ($video.'Date modified').Substring(0,10)
+    $DateModified = ($video.'Date modified').Substring(0, 10)
     $DateModified = [datetime]::parseexact($DateModified, 'dd/MM/yyyy', $null)
     Write-Host " - DateModified ($($DateModified.ToString("yyyy-MM-dd")))" -ForegroundColor DarkYellow
     ##########################################################################
     $FolderNameToCreate = "NODATEINFO"
     ##########################################################################
 
-    if (($video.'Media Created') -ne $null)
-    {
-        $DateTaken = ($video.'Media Created').Substring(0,13)
+    if (($video.'Media Created') -ne $null) {
+        $DateTaken = ($video.'Media Created').Substring(0, 13)
         $DateTaken = Get-AsciiFromUnicode -UnicodeString $DateTaken
 
         #$DateTaken
@@ -259,14 +264,13 @@ foreach ($video in $vidMetadata)
         ##########################################################################
         $FolderNameToCreate = $DateTaken
     }
-    else
-    {           
+    else {           
         Write-Host " - DateTaken (NULL)" -ForegroundColor DarkYellow
 
         if ($DateCreated -gt $DateModified)
-            { $FolderNameToCreate = $DateCreated }
+        { $FolderNameToCreate = $DateCreated }
         else
-            { $FolderNameToCreate = $DateModified}
+        { $FolderNameToCreate = $DateModified }
     }
 
     ################################################################################################
@@ -275,23 +279,39 @@ foreach ($video in $vidMetadata)
     Write-Host "  * FolderToCreate ($($FolderNameToCreate))" -ForegroundColor Yellow
 
     $PathFolderToCreate = "$($folderPath)\$($FolderNameToCreate)"
-    $PathFolderToCreate = $PathFolderToCreate.Replace("\\","\")
+    $PathFolderToCreate = $PathFolderToCreate.Replace("\\", "\")
 
     ################################################################################################
-    if (!(Test-Path -Path $PathFolderToCreate))
-    {
+    if (!(Test-Path -Path $PathFolderToCreate)) {
         if (!($DEBUG))
-            { New-Item -Path $PathFolderToCreate -ItemType directory | Out-Null }
+        { New-Item -Path $PathFolderToCreate -ItemType directory | Out-Null }
         else
-            { Write-Host "  - New-Item -Path $($PathFolderToCreate) -ItemType directory | Out-Null" -ForegroundColor Gray }
+        { Write-Host "  - New-Item -Path $($PathFolderToCreate) -ItemType directory | Out-Null" -ForegroundColor Gray }
     }
 
     ################################################################################################
     Write-Host "  * Copy-Item -Path $($picture.Path) -Destination $($PathFolderToCreate)" -ForegroundColor Yellow
     if (!($DEBUG))
-        { Copy-Item -Path $picture.Path -Destination $PathFolderToCreate }
+    { Copy-Item -Path $picture.Path -Destination $PathFolderToCreate }
+
+    [int] $QtFilesCopied += 1
     ################################################################################################
 
 }
 
-#####################################################################################################################
+
+Write-Host "-------------------------------------------" -ForegroundColor Cyan
+Write-Host "Summary" -ForegroundColor Cyan
+Write-Host "-------------------------------------------" -ForegroundColor Cyan
+Write-Host "QtFiles ($($QtFiles))" -ForegroundColor Cyan
+Write-Host "QtFilesImages ($($QtFilesImages))" -ForegroundColor Cyan
+Write-Host "QtFilesVideos ($($QtFilesVideos))" -ForegroundColor Cyan
+Write-Host "QtFilesOther ($($QtFiles - $QtFilesImages - $QtFilesVideos))" -ForegroundColor Cyan
+Write-Host "QtFilesCopied ($($QtFilesCopied))" -ForegroundColor Cyan
+if ($QtFiles -eq $QtFilesCopied) {
+    Write-Host "OK QtFiles = QtFilesCopied" -ForegroundColor Green
+}
+else {
+    Write-Error "QtFiles != QtFilesCopied"
+}
+Write-Host "-------------------------------------------" -ForegroundColor Cyan
