4e8fdbd93f482df5afe11b468fe3044311924208
Sérgio Fonseca <sergio@fonsecanet.com>
2021-02-09 16:50:24 +0000

    Update

--- Powershell/Azure - Connect Script.ps1
+++ Powershell/Azure - Connect Script.ps1
@@ -3,13 +3,13 @@
     Author: Sergio Fonseca
     Twitter @FonsecaSergio
     Email: sergio.fonseca@microsoft.com
-    Last Updated: 2021-xx-xx
+    Last Updated: 2021-02-09
 
 .SYNOPSIS   
    
 .DESCRIPTION    
  
-.PARAMETER xxxx 
+.PARAMETER SubscriptionName 
        
 #> 
 ########################################################################################################
@@ -35,25 +35,6 @@ else
     }
     else {
         Write-Host "Current subscription is $($Context.Subscription.Name)"    
-    }    
-}
-
-$Context = Get-AzContext
-
-if ($Context -eq $null) {
-    Write-Output "Need to login"
-    Connect-AzAccount -Subscription $SubscriptionName
-}
-else {
-    Write-Output "Context exists"
-    Write-Output "Current credential is $($Context.Account.Id)"
-    if ($Context.Subscription.Name -ne $SubscriptionName) {
-        $Subscription = Get-AzSubscription -SubscriptionName $SubscriptionName -WarningAction Ignore
-        Select-AzSubscription -Subscription $Subscription.Id | Out-Null
-        Write-Output "Current subscription is $($Subscription.Name)"
-    }
-    else {
-        Write-Output "Current subscription is $($Context.Subscription.Name)"    
     }    
 }
 ########################################################################################################
