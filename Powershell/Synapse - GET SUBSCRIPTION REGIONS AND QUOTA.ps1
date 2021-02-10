d75ad79381535a951639aa82f0422d0556e578c3
Sérgio Fonseca <sergio@fonsecanet.com>
2021-02-10 19:29:09 +0000

    update

--- Powershell/Synapse - GET SUBSCRIPTION REGIONS AND QUOTA.ps1
+++ Powershell/Synapse - GET SUBSCRIPTION REGIONS AND QUOTA.ps1
@@ -63,3 +63,70 @@ foreach($location in $synapseLocations)
 }
  
 $quotaResults | ft -AutoSize
+
+
+
+# Get Locations where Synapse is available
+Write-Host "Getting Locations where Synapse is available..."
+$synapseLocations = Get-AzLocation | Where-Object { $_.Providers -contains "Microsoft.Synapse" } | Sort-Object Location | Select-Object Location, DisplayName
+
+# ------------------------------------------------------------------------------
+# get subscription quota and regional available SLOs for Synapse SQL
+
+Write-Host "Getting subscription quota settings for Synapse..."
+$quotaResults = [System.Collections.ObjectModel.Collection[psobject]]@()
+
+foreach($location in $synapseLocations)
+{
+    # ------------------
+    # subscription quota
+    # https://docs.microsoft.com/en-us/rest/api/sql/subscriptionusages/get
+    $subscriptionQuotaUri = "https://management.azure.com/subscriptions/$SubscriptionId/providers/Microsoft.Sql/locations/$($location.Location)/usages/ServerQuota?api-version=2015-05-01-preview"
+    $currentQuotaResult = (ConvertFrom-Json (Invoke-WebRequest -Method Get -Uri $subscriptionQuotaUri -Headers $headers).Content).properties
+    
+    # ------------------------------------
+
+    $quotaResults += [PSCustomObject]@{
+        Location = $location.Location;
+        DisplayName = $location.DisplayName;
+        CurrentServerWorkspaceCount = $currentQuotaResult.currentValue;
+        ServerWorkspaceQuotaLimit = $currentQuotaResult.limit;
+    }
+}
+
+$quotaResults | ft -AutoSize
+
+
+
+
+# ------------------------------------------------------------------------------
+# SQL server DTU limits
+
+Write-Host "Getting SQL DTU limits..."
+
+# all servers in this subscription for SQL
+# https://docs.microsoft.com/en-us/rest/api/sql/servers/list
+# GET https://management.azure.com/subscriptions/{subscriptionId}/providers/Microsoft.Sql/servers?api-version=2019-06-01-preview
+$serversUri = "https://management.azure.com/subscriptions/$SubscriptionId/providers/Microsoft.Sql/servers?api-version=2019-06-01-preview"
+$serverList = (ConvertFrom-Json (Invoke-WebRequest -Method Get -Uri $serversUri -Headers $headers).Content).value
+
+$serverQuotas = @()
+
+foreach($server in $serverList)
+{
+    # usage detail of indiviual server
+    # https://docs.microsoft.com/en-us/rest/api/sql/servers/usages
+    # GET https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{serverName}/usages?api-version=2014-01-01
+    $serverQuotaUri = "https://management.azure.com$($server.id)/usages?api-version=2014-01-01"
+    $serverQuota = (ConvertFrom-Json (Invoke-WebRequest -Method Get -Uri $serverQuotaUri -Headers $headers).Content).value
+
+    $serverQuotas += [PSCustomObject]@{
+        Location = $server.location;
+        Id = $server.id;
+        Name = $server.name;
+        CurrentDTU = ($serverQuota | ? { $_.name -eq 'server_dtu_quota_current' }).currentValue;
+        DTULimit = ($serverQuota | ? { $_.name -eq 'server_dtu_quota_current' }).limit;
+    }
+}
+
+$serverQuotas | Sort-Object Location, Name | ft Location, Name, CurrentDTU, DTULimit -AutoSize
