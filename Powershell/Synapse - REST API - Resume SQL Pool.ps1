7f633138c28a6174bab1ebf9807b799dee28e356
Sérgio Fonseca <sergio@fonsecanet.com>
2021-03-11 17:07:47 +0000

    Update

---
+++ Powershell/Synapse - REST API - Resume SQL Pool.ps1
@@ -0,0 +1,64 @@
+<#   
+.NOTES     
+    Author: Sergio Fonseca
+    Twitter @FonsecaSergio
+    Email: sergio.fonseca@microsoft.com
+    Last Updated: 2021-03-11
+
+.SYNOPSIS   
+   RESUME SYNAPSE SQL POOL
+
+.DESCRIPTION
+    
+#> 
+$ResourceGroup = "SynapseWorkspace"
+$workspaceName = "fonsecanetsynapse"
+$sqlPoolName = "fonsecadwpool"
+$SubscriptionId = "de41dc76-12ed-4406-a032-0c96495def6b"
+
+# ------------------------------------------
+# these Az modules required
+# https://docs.microsoft.com/powershell/azure/install-az-ps
+Import-Module Az.Accounts 
+
+########################################################################################################
+#CONNECT TO AZURE
+
+$Context = Get-AzContext
+
+if ($Context -eq $null) {
+    Write-Information "Need to login"
+    Connect-AzAccount -Subscription $SubscriptionId
+}
+else
+{
+    Write-Host "Context exists"
+    Write-Host "Current credential is $($Context.Account.Id)"
+    if ($Context.Subscription.Id -ne $SubscriptionId) {
+        $result = Select-AzSubscription -Subscription $SubscriptionId
+        Write-Host "Current subscription is $($result.Subscription.Name)"
+    }
+    else {
+        Write-Host "Current subscription is $($Context.Subscription.Name)"    
+    }
+}
+########################################################################################################
+
+# ------------------------------------------
+# get Bearer token for current user for Synapse Workspace API
+$token = (Get-AzAccessToken -Resource "https://management.azure.com").Token
+$headers = @{ Authorization = "Bearer $token" }
+# ------------------------------------------
+
+# https://docs.microsoft.com/en-us/rest/api/synapse/sqlpools/resume
+# POST https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Synapse/workspaces/{workspaceName}/sqlPools/{sqlPoolName}/resume?api-version=2019-06-01-preview
+
+$uri = "https://management.azure.com/subscriptions/$SubscriptionID/"
+$uri += "resourceGroups/$ResourceGroup/providers/Microsoft.Synapse/"
+$uri += "workspaces/$workspaceName/sqlPools/$sqlPoolName/resume?api-version=2019-06-01-preview"
+
+$result = Invoke-RestMethod -Method Post -ContentType "application/json" -Uri $uri -Headers $headers
+
+Write-Host ($result | ConvertTo-Json)
+
+
