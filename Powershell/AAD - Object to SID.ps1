01f5ff2022d047370ef38def175faac80610f634
Sérgio Fonseca <sergio@fonsecanet.com>
2021-09-28 16:17:49 +0100

    Update

---
+++ Powershell/AAD - Object to SID.ps1
@@ -0,0 +1,12 @@
+﻿[string]$objectIdOrAppId = "1d0e6f2f-7be2-4113-bd76-42343fb59f66"
+
+[guid]$guid = [System.Guid]::Parse($objectIdOrAppId)
+
+foreach ($byte in $guid.ToByteArray())
+{
+    $byteGuid += [System.String]::Format("{0:X2}", $byte)
+}
+
+Remove-Variable -Name byteGuid
+
+return "0x" + $byteGuid
