8fba7274d2f04b129a7d3e4e2a5b6e84a9ad2aef
Sérgio Fonseca <sergio@fonsecanet.com>
2022-09-22 11:11:54 +0100

    Update

--- Powershell/AAD - Object to SID.ps1
+++ Powershell/AAD - Object to SID.ps1
@@ -1,3 +1,17 @@
+<#   
+.NOTES     
+    Author: Sergio Fonseca
+    Twitter @FonsecaSergio
+    Email: sergio.fonseca@microsoft.com
+    Last Updated: 2022-09-22
+
+.SYNOPSIS   
+   Function to convert objectid or Application id (Service Principal / MSI) to SID
+
+.DESCRIPTION    
+   Sid can be validated on Synapse with DMV SYS.database_principals / SYS.server_principals  
+#> 
+
 [string]$objectIdOrAppId = "70f02567-10f4-4def-8a46-07c9d1b267c2"
 
 [guid]$guid = [System.Guid]::Parse($objectIdOrAppId)
